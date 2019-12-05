4f453848b5d6355a2dff8a8c58a4948b8fcb0474
Mitchell Baller <mhb91@case.edu>
2019-12-04 21:43:52 -0500

    new file:   request.py Experimenting with python requests

---
+++ request.py
@@ -0,0 +1,19 @@
+import requests
+from requests.exceptions import HTTPError
+
+
+
+#requests' Response instance evaluates to True if status code is between 200 and 400
+
+try:
+    response = requests.get('https://api.github.com')
+    #response = requests.get('https://api.github.com/invalid')
+
+    #raise Exception if response not successful
+    response.raise_for_status()
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
+else:
+    print('Success!')
