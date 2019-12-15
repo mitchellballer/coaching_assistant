b309dbd7d793dc1c11cedcda0a2231f122306a3e
Mitchell Baller <mhb91@case.edu>
2019-12-15 14:07:03 -0500

    javascript calendargit add .1

--- js/calendar.js
+++ js/calendar.js
@@ -1,27 +1,47 @@
-let date = 1;
-for (let i = 0; i < 6; i++){
-    //create a table row
-    let row = document.createElement("tr");
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
 
-    //create cells, populate
-    for (let j = 0; j < 7; j++){
-        if (i == 0 && j<firstDay){
-            cell = document.createElement("td");
-            cellText = document.createTextNode("");
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+showCalendar(currentMonth, currentYear);
+
+function showCalendar(month, year){
+    let firstDay = (new Date(year, month)).getDay();
+
+    tbl = document.getElementById("calendar_body");
+
+    //clear any existing cells/content
+    tbl.innerHTML = "";
+
+    let date = 1;
+    for (let i = 0; i < 6; i++){
+        //create a table row
+        let row = document.createElement("tr");
+
+        //create cells, populate
+        for (let j = 0; j < 7; j++){
+            if (i == 0 && j<firstDay){
+                cell = document.createElement("td");
+                cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth(month, year)) {
+                break;
+            }
+            else {
+                cell = document.createElement("td");
+                cellText = document.createTextNode(date);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
         }
-        else if (date > daysInMonth(month, year)) {
-            break;
-        }
-        else {
-            cell = document.createElement("td");
-            cellText = document.createTextNode(date);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            date++;
-        }
+
+        tbl.appendChild(row);
     }
+}
 
-    toolbar.appendChild(row);
+// check how many days in a month code from https://dzone.com/articles/determining-number-days-month
+function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
 }
