eb1052a7ac972ba036354645d30291cb6b9380bc
Mitchell Baller <mhb91@case.edu>
2020-04-09 03:09:40 -0400

    unable to get OAuth2Session to accept approval_prompt in **kwargs so we just tack it at the end of the authorization url. Seems to function

--- request.py
+++ request.py
@@ -36,24 +36,29 @@ else:
 
 client_id = properties.client_id
 client_secret = properties.client_secret
-redirect_uri = "http://localhost/exchange_token&approval_prompt=force&scope=read"
-scope = "read"
+redirect_uri = 'http://localhost/exchange_token'
+scope = 'read'
+kwargs = {'approval_prompt': 'force'}
+approval_prompt = 'force'
 auth_url = 'https://www.strava.com/oauth/authorize'
 
-oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
+oauth = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri, scope=[scope])
 authorization_url, state = oauth.authorization_url(auth_url)
 
-print 'Please go to %s and authorize access.' % authorization_url
-authorization_response = raw_input('Enter the full callback URL')
+#unable to get **kwargs to accept strava's 'approval_prompt' arg. So we literally just tack it on the end here.
+authorization_url = authorization_url + '&approval_prompt=force'
 
-print authorization_url
+print 'Please go to %s and authorize access.' % authorization_url
+authorization_response = raw_input('Enter the full callback URL: ')
+
 
 """
 oauth.authorization_url returns this url:
 https://www.strava.com/oauth/authorize?response_type=code&client_id=41250&redirect_uri=http%3A%2F%2Flocalhost%2Fexchange_token%26approval_prompt%3Dforce%26scope%3Dread&state=lMpp5o30GyPKst2TZ43HtZGvTAl0bE 
+    generates: http://localhost/exchange_token&approval_prompt=force&scope=read?state=OL8HF4OQRLYTVsGwCfOMgIjcqbYPeA&code=7c08804ea78bbc15001ece4cb66917949cfa264f&scope=
 
 However, that messed up redirect_uri doesn't work. If I substitute the non messed up uri as below, it correctly routes me in the browser.
 https://www.strava.com/oauth/authorize?response_type=code&client_id=41250&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read 
+    generates: http://localhost/exchange_token?state=&code=dba82bf737629169d54b35d2386e0d2a1a48763a&scope=read
 
-Might need to go a little more manual with the url creation here. not sure what the down stream negative effects of this will be...
 """
