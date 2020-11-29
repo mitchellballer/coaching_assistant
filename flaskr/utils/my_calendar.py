1c7f387350f2faa42d12d9437448e2faaa4150c5
Mitchell Baller <mhb91@case.edu>
2020-11-29 13:33:09 -0500

    custom month, week and day object types
    adding a ton of tests next

---
+++ flaskr/utils/my_calendar.py
@@ -0,0 +1,49 @@
+import calendar
+
+
+class Month:
+    # TODO: look into sqlalchemy for storing these objects in my database without a ton of work
+    def __init__(self, year, month):
+        self.year = year
+        self.month = month
+        self.start_day, num_days = calendar.monthrange(year, month)
+        self.has_activities = False
+        self.weeks = []
+        num_weeks = int(((self.start_day + num_days) / 7) - .001)
+        for i in range(num_weeks + 1):
+            self.weeks.append(Week(year, month, i))
+
+    def __str__(self):
+        return f"there are {len(self.weeks)} weeks in this month"
+
+
+class Week:
+    def __init__(self, year, month, week):
+        self.year = year
+        self.month = month
+        self.week = week
+        start_day, num_days = calendar.monthrange(year, month)
+        self.days = []
+
+        if week == 0:
+            for i in range(start_day):
+                self.days.append(Day(year, month, week, 0))
+            count = 1
+            for i in range(start_day, 7):
+                self.days.append(Day(year, month, week, count))
+                count += 1
+        else:
+            count = (7 - start_day) + (7 * (week - 1))
+            while count < count + 7 and count < num_days:
+                self.days.append(Day(year, month, week, count))
+            while count < count + 7:
+                self.days.append(Day(year, month, week, 0))
+
+
+class Day:
+    def __init__(self, year, month, week, day):
+        self.year = year
+        self.month = month
+        self.week = week
+        self.day = day
+
